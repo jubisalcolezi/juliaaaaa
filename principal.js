0dfc80564ac1cf9538707d447b904d6a134c4774
jubisalcolezi <julia.alcolezi@escola.pr.gov.br>
2025-10-02 09:02:51 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -56,6 +56,46 @@
             }
         
             }
+
+            //acessar botão
+            var botaoAdicionar = document.querySelector("#adicionar-paciente");
+            //o que acontece quando clica
+            botaoAdicionar.addEventListener ('click', function(event){
+                event.preventDefault();
+                
+                //acessa formulario
+            var formulario = document.querySelector("#form-adiciona");
+            console.log(formulario);
+            
+            //caça os valores
+            var nome = formulario.nome.value;
+            var peso = formulario.peso.value;
+            var altura = formulario.altura.value;
+            var gordura = formulario.gordura.value;
+            
+            //cria tag Tr
+            var PacienteTr = document.createElement("tr");
+
+            var nomeTd = document.createElement("td");
+            var pesoTd = document.createElement("td");
+            var alturaTd = document.createElement("td");
+            var gorduraTd = document.createElement("td");
+            var imcTd = document.createElement("Td");
+            //adiciona os valores
+                nomeTd.textContent = nome;
+                pesoTd.textContent = peso;
+                alturaTd.textContent = altura;
+                gorduraTd.textContent = gordura;
+                imcTd.textContent = imc;
+
+            });
+
+            
+
+
+
+
+
             
             
             
