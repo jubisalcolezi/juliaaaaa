6ad2bf8e1c48229e490e3dd75de465880bfd6676
jubisalcolezi <julia.alcolezi@escola.pr.gov.br>
2025-10-16 08:57:53 -0300

    Add files via upload

--- formulario.js
+++ formulario.js
@@ -6,17 +6,37 @@ botaoAdicionar.addEventListener ('click', function(event){
     
     //acessa formulario
 var formulario = document.querySelector("#form-adiciona");
-console.log(formulario);
+
+var paciente = buscaValoresFormulario(formulario);
+
+var PacienteTr = criarTr(paciente);
+
+var tabela = document.querySelector("#tabela-pacientes");
+tabela.appendChild(PacienteTr);
+
+});
+
+function buscaValoresFormulario(){
 
 //caça os valores
-var nome = formulario.nome.value;
-var peso = formulario.peso.value;
-var altura = formulario.altura.value;
-var gordura = formulario.gordura.value;
+var paciente = {
+        nome: formulario.nome.value,
+        peso: formulario.peso.value,
+        altura: formulario.altura.value,
+        gordura: formulario.gordura.value,
+        imc: calcularImc(formulario.peso.value,formulario.altura.value)
 
-//cria tag Tr
+}
+
+return paciente;
+
+}
+
+function criarTr(paciente){
+
+    //cria tag Tr
 var PacienteTr = document.createElement("tr");
-
+//cria tag Td
 var nomeTd = document.createElement("td");
 var pesoTd = document.createElement("td");
 var alturaTd = document.createElement("td");
@@ -24,13 +44,13 @@ var gorduraTd = document.createElement("td");
 var imcTd = document.createElement("Td");
 
 //adiciona os valores
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = imc;
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc;
 
-//adiciona as tags na tela do usuário
+    //adiciona as tags na tela do usuário
     PacienteTr.appendChild(nomeTd);
     PacienteTr.appendChild(pesoTd);
     PacienteTr.appendChild(alturaTd);
@@ -40,4 +60,6 @@ var imcTd = document.createElement("Td");
     var tabela = document.querySelector("#tabela-pacientes")
     tabela.appendChild(PacienteTr);
 
-});
+    return PacienteTr;
+
+}
